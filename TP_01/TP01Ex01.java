ff20e993e1671dd06517c3fc55651d30617169b8
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-03-05 20:21:29 -0300

    Created TP01

---
+++ TP_01/TP01Ex01.java
@@ -0,0 +1,17 @@
+//1. Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
+import java.util.Scanner;
+public class TP01Ex01 {
+    public static void main(String args[]){
+        //Instância que captura objetos. 
+        Scanner scan= new Scanner(System.in); 
+        //Capturando num1:
+        System.out.println("Digite a altura do seu retangulo: ");
+        int high= scan.nextInt();
+        //Capturando num2:
+        System.out.println("Digite um numero inteiro:");
+        int base= scan.nextInt();
+        //Exibindo a área do retangulo:
+        int area= (high * base);
+        System.out.println("A base do retangulo: " + area);
+    }
+}
---
+++ TP_01/TP01Ex03.java
@@ -0,0 +1,15 @@
+//3. Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será digitado.
+import java.util.Scanner;
+public class TP01Ex03 {
+    public static void main(String args[]){
+        //Instância que captura objetos. 
+        Scanner scan= new Scanner(System.in); 
+        //Capturando valor da diagonal
+        System.out.println("Digite o valor da diagonal do quadrado: ");
+        int diagonal= scan.nextInt();
+        //
+        int quadrado= scan.nextInt();
+        System.out.println("Digite o valor da diagonal do quadrado: ");
+
+    }
+}
