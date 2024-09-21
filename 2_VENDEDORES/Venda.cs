78cf54e725826ebba40b4c248a3b3e39fc6a0cac
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-09-21 12:26:44 -0300

    doc: created the Project 2 - Vendedores

---
+++ 2_VENDEDORES/Venda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2_VENDEDORES
+{
+    public class Venda
+    {
+        public int Qtde { get; set; }
+        public double Valor { get; set; }
+
+        public Venda(int qtde, double valor)
+        {
+            this.qtde = qtde;
+            this.valor = valor;
+        }
+
+        public double ValorMedio()
+        {
+            return qtde > 0 ? valor / qtde : 0;
+        }
+    }
+}
---
+++ 2_VENDEDORES/Vendedores.cs
@@ -0,0 +1,115 @@
+using System;
+
+namespace _2_VENDEDORES
+{
+    class Vendedores
+    {
+        public Vendedor[] OsVendedores { get; set; }
+        public int Max { get; set; }
+        public int Qtde { get; set; }
+
+        public Vendedores(int max)
+        {
+            Max = max;
+            OsVendedores = new Vendedor[max];
+            Qtde = 0;
+        }
+
+        public bool AddVendedor(Vendedor v)
+        {
+            if (Qtde < Max)
+            {
+                OsVendedores[Qtde++] = v;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DelVendedor(int id)
+        {
+            for (int i = 0; i < Qtde; i++)
+            {
+                if (OsVendedores[i].Id == id)
+                {
+                    if (OsVendedores[i].ValorVendas() == 0)
+                    {
+                        for (int j = i; j < Qtde - 1; j++)
+                        {
+                            OsVendedores[j] = OsVendedores[j + 1];
+                        }
+                        OsVendedores[--Qtde] = null;
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            "Não é possível excluir um vendedor com vendas registradas."
+                        );
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public Vendedor SearchVendedor(int id)
+        {
+            for (int i = 0; i < Qtde; i++)
+            {
+                if (OsVendedores[i] != null && OsVendedores[i].Id == id)
+                {
+                    return OsVendedores[i];
+                }
+            }
+            return null;
+        }
+
+        public double ValorVendas()
+        {
+            double totalVendas = 0;
+            for (int i = 0; i < Qtde; i++)
+            {
+                if (OsVendedores[i] != null)
+                {
+                    totalVendas += OsVendedores[i].ValorVendas();
+                }
+            }
+            return totalVendas;
+        }
+
+        public double ValorComissao()
+        {
+            double totalComissao = 0;
+            for (int i = 0; i < Qtde; i++)
+            {
+                if (OsVendedores[i] != null)
+                {
+                    totalComissao += OsVendedores[i].ValorComissao();
+                }
+            }
+            return totalComissao;
+        }
+
+        public void ListarVendedores()
+        {
+            double totalVendas = 0;
+            double totalComissao = 0;
+
+            Console.WriteLine("Lista de Vendedores:");
+            for (int i = 0; i < Qtde; i++)
+            {
+                if (OsVendedores[i] != null)
+                {
+                    Console.WriteLine(
+                        $"ID: {OsVendedores[i].Id}, Nome: {OsVendedores[i].Nome}, Valor Total de Vendas: {OsVendedores[i].ValorVendas():C}, Comissão: {OsVendedores[i].ValorComissao():C}"
+                    );
+                    totalVendas += OsVendedores[i].ValorVendas();
+                    totalComissao += OsVendedores[i].ValorComissao();
+                }
+            }
+
+            Console.WriteLine($"Total de Vendas: {totalVendas:C}");
+            Console.WriteLine($"Total de Comissões: {totalComissao:C}");
+        }
+    }
+}
