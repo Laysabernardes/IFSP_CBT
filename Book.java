378bb6963054e1ace23687644209df40e6f951fd
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-09-23 22:44:15 -0300

    feat: class book

---
+++ Book.java
@@ -0,0 +1,50 @@
+public class Book {
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author[] authors, double price, int qty){
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    //getters
+    public String getName(){
+        return name;
+    }
+
+    public Author[] getAuthors(){
+        return authors;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getQty(){
+        return qty;
+    }
+
+    //setters
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQty(int qty){
+        this.qty = qty;
+    }
+
+    //O método toString() deve retornar a seguinte resposta: "Book[name=?,authors={Author[name=?,email=?,gender=?],......},price=?,qty=?]".
+    
+    public String toString(){
+        String authorsString = "";
+        for (Author author : authors) {
+            authorsString += author.toString() + ",";
+        }
+        return "Book[name=" + name + ",authors={" + authorsString + "},price=" + price + ",qty=" + qty + "]";
+    }
+
+}
---
+++ TesteBook.java
@@ -0,0 +1,28 @@
+public class TesteBook {
+	public static void main(String[] args) {
+		// Crie e aloque o array de autores
+		Author[] authors = new Author[2];
+		authors[0] = new Author("Autor 01", "autor01@somewhere.com.br", 'm');
+		authors[1] = new Author("Autor 02", "autor02@nowhere.com.br", 'm');
+
+		// Declarar e alocar a instância de um book
+		Book testeBook = new Book("Java for Dummy", authors, 19.99, 99);
+
+		// Testando o método toString
+		System.out.println(testeBook); // Saída esperada: Book[name=Java for Dummy,authors={Author[name=Autor 01,email=autor01@somewhere.com.br,gender=m],Author[name=Autor 02,email=autor02@nowhere.com.br,gender=m]},price=19.99,qty=99]
+
+		// Testando os Getters
+		System.out.println("Nome: " + testeBook.getName());
+		System.out.println("Preço: " + testeBook.getPrice());
+		System.out.println("Quantidade: " + testeBook.getQty());
+		for (Author author : testeBook.getAuthors()) {
+			System.out.println(author);
+		}
+
+		// Testando os Setters
+		testeBook.setPrice(29.99);
+		testeBook.setQty(50);
+		System.out.println("Preço atualizado: " + testeBook.getPrice());
+		System.out.println("Quantidade atualizada: " + testeBook.getQty());
+	}
+}
