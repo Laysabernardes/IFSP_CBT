86cb8d7f73c94669835d3131637aede67b159714
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-09-23 22:22:24 -0300

    feat: class Author

---
+++ Author.java
@@ -0,0 +1,36 @@
+public class Author{
+    //3 atributos privados name (String), email (String), and gender (char contendo 'm' or 'f'); 
+    private String name;
+    private String email;
+    private char gender;
+
+    //um construtor para inicializar os atributos com base nos 3 parâmetros.
+    public Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    
+    //getters
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public char getGender(){
+        return gender;
+    }
+
+    //setter para email
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    //metodo toString
+    public String toString(){
+        return "Author[name=" + name + ", email=" + email + ",gender=" + gender + "]";
+    }
+}
